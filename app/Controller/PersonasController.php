ea239051fc6ef5f25a470f35afdd5d8ed9d9e5fe
Federico Bouzas <federicobouzas@gmail.com>
2013-09-06 17:12:10 -0300

    Fix

--- app/Controller/PersonasController.php
+++ app/Controller/PersonasController.php
@@ -19,6 +19,11 @@ class PersonasController extends AppController {
     }
 
     public function index($last = false) {
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if (isset($this->request->data['Cake']['_output']) && $this->request->data['Cake']['_output'] != "html") {
+                $this->Persona->recursive = -1;
+            }
+        }
         $this->search_list = getData('Personas/PersonasSL');
         parent::index($last);
     }
