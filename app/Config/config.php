16523222d8cb5e56ae07f74026f4d4cac886822d
Federico Bouzas <federicobouzas@gmail.com>
2013-10-22 12:32:11 -0300

    serial

--- app/Config/config.php
+++ app/Config/config.php
@@ -17,6 +17,9 @@ if (!class_exists('AppConfig')) {
     class AppConfig {
 
         public static $array = array(
+            'site' => array(
+                'serial' => 'A88B7-DCD1A-9E3E1-7770B-BAA6D-7515B-31A2D-7E85D'
+            ),
             'users' => array(
                 'login' => array(
                     'ban' => true,
